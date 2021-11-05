6b4639ce410aa4324f9bab5f1d90dac81084fa7b
Kiryl Bahdanovich <Kiryl_Bahdanovich@epam.com>
2021-11-05 20:12:12 +0300

    fixed SonarQube error

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace WebApplication1
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
